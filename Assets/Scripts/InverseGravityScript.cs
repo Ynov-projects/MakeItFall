d92a269620073980461bc8d63b1d35bbc2d581f2
gaelboisson <gael.boisson.77@gmail.com>
2024-04-30 11:58:03 +0200

    Modification de l'inversion de gravité + détails

--- Assets/Scripts/InverseGravityScript.cs
+++ Assets/Scripts/InverseGravityScript.cs
@@ -8,19 +8,8 @@ public class InverseGravityScript : MonoBehaviour
     [SerializeField] private float _pushForce;
     [SerializeField] private Rigidbody _rb;
 
-    // Start is called before the first frame update
-    void Start()
+    public void ReverseAllGravity()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            //_rb.AddForce(new Vector3(0, _pushForce, 0));
-            Physics.gravity = new Vector3(0, Physics.gravity.y > 1f ? - _gravityForceY : _gravityForceY, 0);
-        }
+        Physics.gravity = new Vector3(0, Physics.gravity.y > 1f ? -_gravityForceY : _gravityForceY, 0);
     }
 }
